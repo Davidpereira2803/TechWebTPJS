c681d11f4b6f8f602bea6ac3a7b163bd3411f281
Davidpereira2803 <david.pereira.002@student.uni.lu>
2024-05-02 22:37:47 +0200

    final

--- README.md
+++ README.md
@@ -9,3 +9,9 @@ Le projet a 3 fichier principaux:
 * style.css -> contient le code CSS pour rendre la page web plus belle
 
 * script.js -> contient JavaScript pour pouvoir ajouter, supprimer et bouger les livres d'une liste à l'autre
+
+## Fonctionement
+
+Afin de pouvoir bouger les livres on utilise des events de JavaScript et on donne un paramètre draggable comme true aux livres pour dire qu'ils peuvent être selectioné avec la souris et bouger. Pour pouvoir déposer les livres il faut donner un event à la place ou on veut les déposer. Dans notre cas un div qui represente un carreau dans la page web. 
+
+Pour les livres on a une liste qui contient les livres et on peut ajouter des livres par un form sur la page web, qui rajoute un livre à la liste, on peut aussi supprimer les livres avec un button dans la case de chaque livre. Pour ajouter un livre on doit remplir les 4 inputs, name, author, price et id. Quand un livre et déplacer on change son status de vente. 
--- script.js
+++ script.js
@@ -1,17 +1,25 @@
+// liste avec les livres, nom, id, auteur, prix, et status de vente
 let books = [
     {id: 1, name: "Harry Potter", author: "David Pereira", price: 10, sold: false},
     {id: 2, name: "Harry Potter 2", author: "David Pereira", price: 8, sold: false},
     {id: 3, name: "Harry Potter 3", author: "David Pereira", price: 20, sold: true},
 ];
 
+// listes qui representent les livres disponibles et vendus
 let $availableList = $("#availableList");
 let $soldList = $("#soldList");
 
-
-const renderBooks = () => {
+// updateBooks est une fonction qui permet de mettre les livres de la liste dans le HTML
+const updateBooks = () => {
+    // Il faut a chaque fois mettre les listes vides
     $availableList.empty();
     $soldList.empty();
 
+    /* 
+        Pour chaque livre dans books on crée un div avec la classe "book" après on implemente un 
+        div avec le contenu du livre actuel (book), puis on ajoute le button Delete qui declanche la 
+        fonction deleteBook(id)
+    */
     books.forEach(function (book) {
         const $div = $("<div>")
             .addClass("book")
@@ -30,15 +38,10 @@ const renderBooks = () => {
             })
             .appendTo($div);
     });
-
-    $(".delete").click(function () {
-        const bookId = $(this).parent().attr("book");
-        deleteBook(bookId);
-    });
 }
 
-
-const addBehaviorToBook = (book) => {
+// bookBehavior permet au livres d'etre bouger et placer 
+const bookBehavior = (book) => {
     if (!book.id) {
         book.id = window.crypto.randomUUID()
     }
@@ -49,64 +52,89 @@ const addBehaviorToBook = (book) => {
 
     book.addEventListener('dragend', (event) => {
         event.preventDefault()
+        if(book.sold == false) book.sold = true;
+        else book.sold = false;
     })
 }
 
-const addBehaviorToPanel = (panel) => {
-    panel.addEventListener('dragover', (event) => {
+// boxBehavior permet de poser les livres
+const boxBehavior = (box) => {
+    box.addEventListener('dragover', (event) => {
         event.preventDefault()
     })
 
-    panel.addEventListener('drop', (event) => {
+    box.addEventListener('drop', (event) => {
         console.log(event)
-        const dragged_card_id = event.dataTransfer.getData("book_1")
-        console.log(dragged_card_id)
+        const dragged_book_id = event.dataTransfer.getData("book_1")
+        console.log(dragged_book_id)
 
-        event.target.appendChild(document.getElementById(dragged_card_id))
+        event.target.appendChild(document.getElementById(dragged_book_id))
     })
 }
 
+
+// addBook permet d'ajouter un livre a la liste des disponibles
 function addBook(event) {
     event.preventDefault();
 
+    // On défine les valeurs du livres, pour cela on prend les valeurs des inputs avec les classes suivantes
     const name = $("#bookName").val();
     const author = $("#bookAuthor").val();
     const price = $("#bookPrice").val();
     const id = $("#bookId").val();
 
-    books.push({id, name, author, price, sold: false});
-    renderBooks();
+    // on rajoute le nouveu livre et on met la liste à jour
+    if(name != "" && author != "" && price !="" && id !=""){
+        books.push({id, name, author, price, sold: false});
+        updateBooks();
+    }
 
+    // On doit mettre le paramètre du nouveau livre 'draggable' comme true pour pouvoir le bouger
     const newBook = $(`[book=${id}]`)[0]; 
     newBook.setAttribute('draggable', true); 
-    addBehaviorToBook(newBook);
+    bookBehavior(newBook);
+    
 
-    $("#book-name").val("");
-    $("#book-author").val("");
-    $("#book-price").val("");
+    // On remet les valeurs des inputs à vide pour le prochain livre
+    $("#bookName").val("");
+    $("#bookAuthor").val("");
+    $("#bookPrice").val("");
+    $("#bookId").val("");
 }
 
+
+// deleteBook permet de supprimer un livre par un button
 function deleteBook(id) {
+    // On prend le livre de la liste avec l'id passer comme parametre
     const index = books.findIndex(book => book.id === id);
+    // Si cette id existe -> different de -1, on enleve le livre et on met à jour la liste
     if (index !== -1) {
         books.splice(index, 1);
-        renderBooks();
+        updateBooks();
 
-        const remainingBooks = document.querySelectorAll(".book");
-        remainingBooks.forEach(addBehaviorToBook);
+        /*
+            Après avoir modifier la liste il faut appeler la fonction bookBehavior pour tous les livres
+            afin d
+            */ 
+        const newList = document.querySelectorAll(".book");
+        newList.forEach(bookBehavior);
     }
 }
 
+// main est appele apres que la page est completement contruite
 const main = () => {
-    renderBooks();
-    const wbooks = document.querySelectorAll(".book")
-    wbooks.forEach((book) => {
+    // initialiser les livres
+    updateBooks();
+
+    // il faut donner l'attribue draggable comme true pour pouvoir bouger les elements(livres)
+    const booksList = document.querySelectorAll(".book")
+    booksList.forEach((book) => {
         book.setAttribute('draggable', true)
     })
-    wbooks.forEach(addBehaviorToBook)
+    booksList.forEach(bookBehavior)
 
     const panels = document.querySelectorAll(".box")
-    panels.forEach(addBehaviorToPanel)
+    panels.forEach(boxBehavior)
 }
 
 document.addEventListener('DOMContentLoaded', main)
